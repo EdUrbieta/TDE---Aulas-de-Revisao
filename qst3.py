210a7b9d4f4e2b8db78ca97ae7149ad6de0bc08d
Eduardo Urbieta <92870959+BonbuBot@users.noreply.github.com>
2022-10-03 23:33:27 -0300

    Create qst3.py

---
+++ qst3.py
@@ -0,0 +1,7 @@
+# Questão 3
+
+fahrenheit = float(input("Digite a temperatura em Fahrenheit a ser convertida para Celsius: "))
+
+celsius = (fahrenheit - 32) * 5/9
+
+print(f"{fahrenheit} em Fahrenheit equivale a {round(celsius, 2)} em Celsius.")
