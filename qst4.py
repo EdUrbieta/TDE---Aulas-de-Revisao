a5e181db0d38650dad7aea0c56a9da93f7d7ace8
Eduardo Urbieta <92870959+BonbuBot@users.noreply.github.com>
2022-10-03 23:33:49 -0300

    Create qst4.py

---
+++ qst4.py
@@ -0,0 +1,9 @@
+# Questão 4
+
+ano = int(input("Informe o ano a ser checado: "))
+
+if (ano % 4 == 0 and ano % 100 != 0 or ano % 400 == 0):
+    print("Este ano é um ano bissexto.")
+
+else:
+    print("Este ano não é um ano bissexto.")
